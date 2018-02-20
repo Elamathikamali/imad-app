10f2166f58c817f67f716d1797d4d51b8bba509e
Elamathikamali <36579981+Elamathikamali@users.noreply.github.com>
2018-02-20 17:06:37 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/modi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'modi.png'));
+app.get('/ui/moai.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
